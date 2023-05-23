3641f7463d2e68948639b19b5e63efc9d5985306
petre92 <petre.chikviladze@gmail.com>
2023-05-23 19:19:57 +0400

    update

--- src/main/java/DataObject/SearchBoxData.java
+++
@@ -1,6 +0,0 @@
-package DataObject;
-public interface SearchBoxData {
-    String correctProductName = "ჩეკის ლურსმანი";
-    String incorrectProductName = "nina";
-    String errorSearchMessage = "საძიებო სიტყვა" + "'"+ incorrectProductName + "'" + "ვერაფერი მოიძებნა";
-}
